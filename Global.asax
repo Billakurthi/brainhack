2cf47c3c6d0dfcebfa8f0f5a07f0c389a7921404
billakurthi <avinash.billakurthi@gmail.com>
2017-06-06 13:34:55 +0530

    new

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="brainHack.Global" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(brainHack.Startup))]
+namespace brainHack
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
