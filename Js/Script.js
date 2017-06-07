f2229a773866c317f01ba1d2281ae14ba7581c12
billakurthi <avinash.billakurthi@gmail.com>
2017-06-07 16:47:07 +0530

    giphy added

--- Js/Script.js
+++ Js/Script.js
@@ -12,7 +12,19 @@
             
         $http.get('http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC').
                     then(function(response) {
-                       
+                        var results = response.data.data;
+
+                        $scope.results = results;
+
+                        //var resultsLength = results.length;
+
+                        //if (resultsLength) {
+
+                        //    for (var i = 0 ; i < resultsLength ; i++)
+                        //    {
+                        //        console.log(results[i].id.toString());
+                        //    }
+                        //}
 
                         $scope.firstName = response.data.content;
                         $scope.lastName = response.data.id;
--- Projects/GiphySearch.aspx
+++ Projects/GiphySearch.aspx
@@ -5,6 +5,14 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
 
     Giphy Search Page
+    <div class="row">
+        
+        <div class="col-xs-6 col-sm-4 col-md-3" data-ng-repeat="result in results">
+            <img style="width:100%; height:auto;" src="{{result.images.downsized.url}}" alt="{{result.type}}" title="{{result.type}}" />
+        </div>         
+
+        
+    </div>
 
 </asp:Content>
 
